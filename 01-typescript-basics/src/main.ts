f7514c517466694978c5cffe45d8e38563a50dd8
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-14 11:24:59 +0100

    feat: project start

---
+++ 01-typescript-basics/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+hola mundo
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
