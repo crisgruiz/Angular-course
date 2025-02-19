bf335e7820e98d218803a476fe5b0d11359904c1
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-19 14:34:23 +0100

    feat: generics types

--- 01-typescript-basics/src/main.ts
+++ 01-typescript-basics/src/main.ts
@@ -1,9 +1,9 @@
-import './style.css'
-import './topics/08-classes.ts'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import "./topics/09-generics.ts";
+import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 hola mundo
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
