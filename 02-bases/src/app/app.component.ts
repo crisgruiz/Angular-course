c4bdd52a70c86a95ade40a5528a8bae5eae83339
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-20 16:41:55 +0100

    feat: creating functions and implementing them

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -9,4 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Mi primera app de Angular';
   public counter: number = 10;
+
+  increaseBy = (value: number): void => {
+    this.counter += value;
+  };
+
+  resentCounter = (): void => {
+    this.counter = 10;
+  };
 }
