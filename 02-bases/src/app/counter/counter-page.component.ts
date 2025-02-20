2c817d0c72d0a9c98b967de0d5bb7e2c268703b8
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-20 22:07:37 +0100

    feat(counter): intruduce signals

--- 02-bases/src/app/counter/counter-page.component.ts
+++ 02-bases/src/app/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 // @Component() is a decorator that turns my class into a component
 @Component({
   templateUrl: './counter-page.component.html',
@@ -9,15 +9,21 @@ import { Component } from '@angular/core';
       width: 75px;
     }
   `,
+  standalone: false,
+  selector: 'app-counter',
 })
 export class CounterComponent {
   public counter: number = 10;
+  counterSignal = signal(10);
 
   increaseBy = (value: number): void => {
     this.counter += value;
+    // this.counterSignal.set(this.counterSignal() + value);
+    this.counterSignal.update((current) => current + value);
   };
 
   resetCounter = (): void => {
     this.counter = 10;
+    this.counterSignal.set(10);
   };
 }
