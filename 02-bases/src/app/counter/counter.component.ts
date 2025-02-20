247b4489216dff9115388d9c4ba4c390513df644
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-20 16:58:32 +0100

    refactor: move counter functionality to component

--- 02-bases/src/app/counter/counter.component.ts
+++ 02-bases/src/app/counter/counter.component.ts
@@ -2,7 +2,21 @@ import { Component } from '@angular/core';
 // @Component() is a decorator that turns my class into a component
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>',
+  template: `<p>Counter: {{ counter }}</p>
+
+    <button (click)="increaseBy(+1)">+1</button>
+    <button (click)="resentCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>`,
   standalone: false,
 })
-export class CounterComponent {}
+export class CounterComponent {
+  public counter: number = 10;
+
+  increaseBy = (value: number): void => {
+    this.counter += value;
+  };
+
+  resentCounter = (): void => {
+    this.counter = 10;
+  };
+}
