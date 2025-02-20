7937d75f7af0c4c70e4ec1893d5d762f4ac6b2b4
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-20 21:59:23 +0100

    feat: remove list component

--- 02-bases/src/app/heroes/list/list.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-heroes-list',
-  standalone: false,
-  templateUrl: './list.component.html',
-  styleUrl: './list.component.css',
-})
-export class ListComponent {}
