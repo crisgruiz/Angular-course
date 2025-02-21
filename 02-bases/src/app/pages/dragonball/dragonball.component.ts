951d4cc03f405cd960ad3678039c89d1dc4ca158
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-21 12:57:20 +0100

    feat(dragonball): inputs exercise

--- 02-bases/src/app/pages/dragonball/dragonball.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball.component.ts
@@ -13,6 +13,9 @@ interface Character {
   // imports: [NgClass],
 })
 export class DragonballComponent {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
     {
       id: 1,
@@ -35,6 +38,26 @@ export class DragonballComponent {
       power: 500,
     },
   ]);
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    // this.characters().push(newCharacter);
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+  resetFields = () => {
+    this.name.set('');
+    this.power.set(0);
+  };
 
   // powerClasses = computed(() => {
   //   return {
