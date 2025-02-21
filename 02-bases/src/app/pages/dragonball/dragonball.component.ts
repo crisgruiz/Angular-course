81a204334d84fadb61a285802e22c32b529f21c6
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-21 11:41:50 +0100

    feat(dragonball): add data and list logic

--- 02-bases/src/app/pages/dragonball/dragonball.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
-  selector: 'app-dragonball',
-  standalone: false,
   templateUrl: './dragonball.component.html',
-  styleUrl: './dragonball.component.css'
+  styleUrl: './dragonball.component.css',
 })
 export class DragonballComponent {
-
+  characters = signal<Character[]>([
+    {
+      id: 1,
+      name: 'Goku',
+      power: 9000,
+    },
+    {
+      id: 2,
+      name: 'Vegeta',
+      power: 8000,
+    },
+    {
+      id: 3,
+      name: 'Piccolo',
+      power: 3000,
+    },
+  ]);
 }
