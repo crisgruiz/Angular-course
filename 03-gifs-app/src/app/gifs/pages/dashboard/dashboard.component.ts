b38f4e166481519bba843f0b53ef0031dbc89f11
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-26 15:34:39 +0100

    feat(pages): create pages

--- 03-gifs-app/src/app/gifs/pages/dashboard/dashboard.component.ts
+++ 03-gifs-app/src/app/gifs/pages/dashboard/dashboard.component.ts
@@ -5,4 +5,4 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent {}
+export default class DashboardComponent {}
