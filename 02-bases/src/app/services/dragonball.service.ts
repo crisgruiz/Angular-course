1cb748e3cd6e7c77c542783a8dde476c2050e9c7
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-26 11:37:30 +0100

    feat(dragonball.service): introduce effect and localstorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,20 +1,32 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+};
+
 //injectable transform the class to a service
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
-  characters = signal<Character[]>([
-    {
-      id: 1,
-      name: 'Goku',
-      power: 9001,
-    },
-    {
-      id: 2,
-      name: 'Vegeta',
-      power: 9000,
-    },
-  ]);
+  // characters = signal<Character[]>([
+  //   {
+  //     id: 1,
+  //     name: 'Goku',
+  //     power: 9001,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Vegeta',
+  //     power: 9000,
+  //   },
+  // ]);
+
+  characters = signal<Character[]>(loadFromLocalStorage());
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
   addCharacter(newCharacter: Character) {
     this.characters.update((list) => [...list, newCharacter]);
