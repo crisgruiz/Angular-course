c1d057cf8c85b917d0b72cd279c8002a9527d2e2
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-02-26 16:00:07 +0100

    refactor(dashboard): create menu components

--- 03-gifs-app/src/app/gifs/pages/dashboard/dashboard.component.ts
+++ 03-gifs-app/src/app/gifs/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuHeaderComponent } from '../../components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component';
+import { SideMenuOptionsComponent } from '../../components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component';
+import { SideMenuComponent } from '../../components/gifs-side-menu/gifs-side-menu.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuComponent],
   templateUrl: './dashboard.component.html',
 })
 export default class DashboardComponent {}
