1ba7c036618314d4fa02483070febb5b439ddbf8
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-03-05 12:44:24 +0100

    feat(env): add giphyUrl

--- 03-gifs-app/src/environments/environment.ts
+++ 03-gifs-app/src/environments/environment.ts
@@ -5,5 +5,6 @@ export const environment = {
   slogan: 'Manage your actions and activities',
   //APIKeys
   giphyApiKey: 'b9WPVAGoE5q4sXrOUC01AD2vHRXgYkWZ',
+  giphyUrl: 'https://api.giphy.com/v1',
   //URLS
 };
