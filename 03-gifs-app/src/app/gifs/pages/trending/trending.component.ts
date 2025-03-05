0745b7a3b9801df8f5565b1d0c4820ab010bb15b
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-03-05 12:01:42 +0100

    feat(trending): create list components

--- 03-gifs-app/src/app/gifs/pages/trending/trending.component.ts
+++ 03-gifs-app/src/app/gifs/pages/trending/trending.component.ts
@@ -1,8 +1,9 @@
+import { GifListComponent } from './../../../../../../02-bases/src/app/components/gif-list/gif-list.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-trending',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './trending.component.html',
 })
 export default class TrendingComponent {}
