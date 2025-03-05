dcdc5116761bd8b9a29eefbd0af82880b89e6d19
Cristina Gonzalez <cristinagonzalez@MacBook-Pro-de-Cristina.local>
2025-03-05 11:31:18 +0100

    feat(menu-options): implement routerLink

--- 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ 03-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  sublabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent {}
+export class SideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ];
+}
